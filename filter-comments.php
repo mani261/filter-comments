d7739973e610f977fb3b98931758129c34dc0ac9
MaNi <mani.razavizadeh@gmail.com>
2022-12-01 12:20:20 +0330

    page menu

--- filter-comments.php
+++ filter-comments.php
@@ -10,7 +10,7 @@ Domain Path: /languages/
 Version: 1.0.0
  */
 
-if (!defined('ABSPATH')) {
+if (!defined ('ABSPATH')) {
     exit;
 }
 
@@ -22,13 +22,32 @@ define('WP_AUTH_TPL', WP_AUTH_DIR . '/tpl/');
 include WP_AUTH_INC . "functions.php";
 include WP_AUTH_INC . "shortcodes.php";*/
 
-function comment_text_filter(string $comment_body):string {
-    $filter_words = ['moderating','the'];
+add_action ('admin_menu', 'menu_handler');
+
+function menu_handler ()
+{
+    add_menu_page ('Filter Comments', 'Filter Comments', 'manage_options',
+        'filter-comments', 'filter_comment_main_handler', 'dashicons-format-chat',26);
+    add_submenu_page ('filter-comments', 'Settings', 'Settings', 'manage_options', 'settings', 'filter_comment_settings_handler');
+}
+
+function filter_comment_main_handler(){
+    echo 'From Main ';
+}
+
+function filter_comment_settings_handler(){
+    echo 'settings';
+}
+
+function comment_text_filter (string $comment_body): string
+{
+    $filter_words = ['moderating', 'the'];
     foreach ($filter_words as $word) {
-        $word_len = strlen($word);
-        $word_filtered = str_repeat('*',$word_len);
-        $comment_body = str_replace($word,$word_filtered,$comment_body);
+        $word_len = strlen ($word);
+        $word_filtered = str_repeat ('*', $word_len);
+        $comment_body = str_replace ($word, $word_filtered, $comment_body);
     }
     return $comment_body;
 }
-add_filter('comment_text', 'comment_text_filter');
+
+add_filter ('comment_text', 'comment_text_filter');
